40385c8c4cc0a97873aa3d5d716cff14248ff938
Gowtham-arja <kalyanarja61@gmail.com>
2026-01-23 21:07:40 +0530

    fix: update base path to lowercase for consistency

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base: '/Sorting-Algorithm-Visualizer/',
+  base: '/sorting-algorithm-visualizer/',
 })
